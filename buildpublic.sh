1f3b247ce01f3115fd102564260de9045f68c5c3
Victor Puiu <victor.puiu01@gmail.com>
2022-03-14 16:06:26 +0000

    update builder script

--- buildpublic.sh
+++ buildpublic.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+echo "Pulling from remote..."
 git pull
+echo "Cleaning build & /var/www/artools folder..."
+rm -rf build
+sudo rm -rf /var/www/artools
+echo "Building artools..."
 npm run build
+echo "Moving files to public directory..."
 cd build
-mv * /var/www/artools .
+sudo mkdir /var/www/artools
+sudo mv * /var/www/artools .
+echo "Applying permissions..."
 sudo chmod -R 755 /var/www/artools
 sudo chown -R www-data:www-data /var/www/artools
 
+echo "Ready!"
